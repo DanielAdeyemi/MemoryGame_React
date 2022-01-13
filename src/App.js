f2cb01c2edbcc3aeae025178cfb5210b120efadc
DanielAdeyemi <adeyemidany@gmail.com>
2022-01-13 15:13:23 -0800

    create shuffle function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import "./App.css";
 
+const cardImg = [
+	{ src: "/img/helmet-1.png" },
+	{ src: "/img/potion-1.png" },
+	{ src: "/img/ring-1.png" },
+	{ src: "/img/scroll-1.png" },
+	{ src: "/img/shield-1.png" },
+	{ src: "/img/sword-1.png" },
+];
+
 function App() {
+	const shuffleCards = () => {
+		const shuffleCards = [...cardImg, ...cardImg] // create an array with 2 cards of each type
+			.sort(() => Math.random() - 0.5) // sort and randomize an order (shuffle)
+			.map((card) => ({ ...card, id: Math.random() })); // assign an id to each card
+	};
+
 	return (
 		<div className="App">
 			<h1>Magic Match</h1>
