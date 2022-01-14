0c7069772d06d6f56846b9fbe78d29d6a0eb9781
DanielAdeyemi <adeyemidany@gmail.com>
2022-01-14 10:54:04 -0800

    update card styles

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,11 +1,11 @@
 import "../styles/SingleCard.scss";
 
-const SingleCard = (card) => {
+const SingleCard = ({ card }) => {
 	return (
 		<>
-			<div className="card" key={card.id}>
+			<div className="card">
 				<div>
-					<img className="front" src={card.card.src} alt="card front" />
+					<img className="front" src={card.src} alt="card front" />
 					<img className="back" src="/img/cover.png" alt="card back" />
 				</div>
 			</div>
